e56c8d43102fa1afd4287080e52adc50082d4e90
rabiixx <rabiixx@gmail.com>
2020-01-04 10:05:17 +0100

    api inplementada

--- CO2calculator/catalog/static/js/main.js
+++ CO2calculator/catalog/static/js/main.js
@@ -3,7 +3,7 @@ alert("Hello from js1");
 axios.get("https://api.triptocarbon.xyz/v1/footprint?activity=10&activityType=miles&country=usa&mode=taxi")
 .then(function (response) {
     console.log(response.data);
-    //document.getElementById("textConsumo").innerHTML = response.data;
+    document.getElementById("textConsumo").innerHTML = response.data['carbonFootprint'];
 })
 .catch(function (error) {
     console.log(error);
