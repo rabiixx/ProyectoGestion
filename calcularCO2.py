8630bccf38ba7652d55295e8902e398dac3b3ead
sabi <55583233+sabitopito@users.noreply.github.com>
2020-01-06 22:29:07 +0100

    Delete calcularCO2.py

--- calcularCO2.py
+++
@@ -1,8 +0,0 @@
-import json
-from urllib.request import urlopen
-
-
-api_url = 'https://api.triptocarbon.xyz/v1/footprint?activity=10&activityType=miles&country=usa&mode=taxi'
-response = urlopen(api_url)
-data = json.loads(response.read())
-print(data['carbonFootprint'])
